367bf70b006bb3ff553d90db6bfcedc46d8d0abc
sahilhaq2003 <sahilhaq2003@gmail.com>
2025-06-23 22:13:19 +0530

    add a download report button

--- frontend/src/Components/User/User.js
+++ frontend/src/Components/User/User.js
@@ -28,4 +28,5 @@ function User({ user }) {
     </div>
   );
 }
+
 export default User
--- frontend/src/Components/UserDetails/Users.js
+++ frontend/src/Components/UserDetails/Users.js
@@ -1,31 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import User from '../User/User';
-const URL = 'http://localhost:5000/users';
+import { useReactToPrint } from 'react-to-print';
+import './Users.css';
 
+const URL = 'http://localhost:5000/users';
 
 const fetchHandler = async () => {
   return await axios.get(URL).then((res) => res.data);
 }
+
 function Users() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     fetchHandler().then((data) => setUsers(data.users));
   }, []);
 
+  const ComponentsRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => ComponentsRef.current,
+    documentTitle: 'User Report',
+    onAfterPrint: () => alert('User Report Successfully Downloaded!'),
+  });
 
   return (
     <div>
       <h1>User Details Page</h1>
-      <div>
-        {users && users.map((user, i) => (
-          <div key={i}>
-            <User user={user} />
-          </div>
-        ))}
+      <div ref={ComponentsRef}>
+        {users && users.length > 0 ? (
+          users.map((user, i) => (
+            <div key={i}>
+              <User user={user} />
+            </div>
+          ))
+        ) : (
+          <div>No users to print</div>
+        )}
+
       </div>
+      <button className= "download-button" onClick={handlePrint}>Download Report</button>
     </div>
-  )
+  );
 }
 
-export default Users
+export default Users;
